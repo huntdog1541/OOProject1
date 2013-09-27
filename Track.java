491e5ee41b8ffce746d491f99b192b9a2298f27c
David Hunt <huntdog1541@gmail.com>
2013-09-26 20:55:34 -0500

    Create Track.java

---
+++ Track.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+
+public class Track {
+
+	private static final long serialVersionUID = 7443883203941320122L;
+
+	Rectangle2D startLine;
+	Rectangle2D finishLine;
+	
+	public Track()
+	{
+		startLine = new Rectangle(100, 150, 50, 50);
+		finishLine = new Rectangle(700, 150, 50, 50);
+	}
+	
+	public void draw(Graphics g)
+	{
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.draw(startLine);
+		g2.draw(finishLine);
+		g2.drawString("Finish", 705, 400);
+		g2.drawString("Start", 120, 450);
+	}
+
+}
