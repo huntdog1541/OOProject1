9f90900a54139045df8db2c148d45a65f2e9d483
David Hunt <huntdog1541@gmail.com>
2013-09-26 20:25:38 -0500

    Update Run.java

--- Run.java
+++ Run.java
@@ -17,11 +17,22 @@ public class Run {
 		
 		frame.setVisible(true);
 		
-		while( x != 10)
+		while( x != 100)
 		{
-		r1.moveCars(1);
+		r1.moveCars();
 		x++;
+		
+		try {
+		    Thread.sleep(10);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		    // handle the exception...        
+		    // For example consider calling Thread.currentThread().interrupt(); here.
 		}
+		
+		}
+		
+		r1.printX(1);
 		
 	}
 
