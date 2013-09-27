f8e3be4cf7a05bdd7d26437f0523dc6269b33e65
David Hunt <huntdog1541@gmail.com>
2013-09-27 13:21:29 -0500

    Update Blimp.java

--- Blimp.java
+++ Blimp.java
@@ -7,24 +7,37 @@ import java.awt.geom.Ellipse2D;
 public class Blimp {
 
 	Ellipse2D blimp;
+	private int blimpX;
+	private int blimpY;
+	private int WordsX;
+	private int WordsY;
 	
 	public Blimp()
 	{
-		
+		blimpX = 50;
+		blimpY = 50;
+		WordsX = 65;
+		WordsY = 85;
 	}
 	
 	public void draw(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		System.out.println(" Draw car ");
-		Ellipse2D.Double blimp1 = new Ellipse2D.Double(50, 50, 100, 60);
+		
+		Ellipse2D.Double blimp1 = new Ellipse2D.Double(blimpX, blimpY, 100, 60);
 		
 		g2.setColor(Color.blue);
 		g2.draw(blimp1);
 		g2.fill(blimp1);
 		
 		g2.setColor(Color.yellow);
-		g2.drawString("GOODYEAR", 65, 85);
+		g2.drawString("GOODYEAR", WordsX, WordsY);
 		
+	}
+	
+	public void blimpMove()
+	{
+		blimpX = blimpX + 2;
+		WordsX = WordsX + 2;
 	}
 }
