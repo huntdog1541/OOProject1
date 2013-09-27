10be3ff33c20dc87b51df96958d60bfc4e28af3e
David Hunt <huntdog1541@gmail.com>
2013-09-26 19:48:33 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,76 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.lang.Math;
+
+
+@SuppressWarnings("unused")
+public class Car 
+{
+	private int x;
+	private int y;
+	private Color carColor;
+	private int numberOfRaces;
+	private int numberOfWins;
+	private double percentOfWins;
+	
+	public Car()
+	{
+		x = 100;
+		y = 100;
+	}
+	
+	public Car(int VarX, int VarY, Color cc)
+	{
+		x = VarX;
+		y = VarY;
+		carColor = cc;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Color getCarColor() {
+		return carColor;
+	}
+
+	public void setCarColor(Color carColor) {
+		this.carColor = carColor;
+	}
+
+	public void moveCar()
+	{
+		int nX = 0;
+		nX = (int)Math.random()*10;
+		x = x + nX;
+	}
+	
+	public void draw(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		
+		Rectangle body = new Rectangle(x, y + 10, 60, 10);
+		g2.setColor(carColor);
+		g2.fillRect(x, y + 10, 60, 10);
+		Ellipse2D.Double frontTire = new Ellipse2D.Double(x + 10, y + 20, 10, 10);
+		Ellipse2D.Double rearTire = new Ellipse2D.Double(x + 40, y + 20, 10, 10);
+		
+		g2.drawArc(x + 15, y, 30, 20, 0, 180);
+		
+		g2.draw(body);
+		g2.draw(frontTire);
+		g2.draw(rearTire);
+	}
+}
